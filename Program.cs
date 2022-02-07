220e39cd6899f22cab1b616922a2af68dcbd0d0a
Dr. Deckto <fynjy138@gmail.com>
2022-02-07 03:38:19 +0300

    реализован двусвязный список

--- Program.cs
+++ Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            TestsToList();
+        }
+
+        private static void TestsToList() //как по узлу искать я понял, но немного не допер как проверить.
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.Add(2);
+            list.Add(4);
+            list.Add(8);
+            list.Add(12);
+            list.Add(14);
+            list.Add(16);
+            list.Add(17);
+
+            Console.WriteLine($"Количество элементов в списке - {list.GetCount()}");
+
+            Console.WriteLine($"Элементы списка : ");
+            list.OutputAllItems();
+
+            list.RemoveNodeByValue(2);
+            list.OutputAllItems();
+
+            list.RemoveNodeByIndex(3);
+            list.OutputAllItems();
+
+            try
+            {
+                list.RemoveNodeByIndex(12);//упадёт
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            var tempValue = list.FindNode(14);
+
+            Console.WriteLine(tempValue.Value);
+
         }
     }
 }
