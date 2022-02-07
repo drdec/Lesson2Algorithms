4f7113da060c3a37877e97e03e393a4a024c23cb
Dr. Deckto <fynjy138@gmail.com>
2022-02-07 03:50:58 +0300

    реадтзован бин поиск

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace Lesson2
         private static void Main()
         {
             TestsToList();
+
+            Console.ReadKey();
+
+            TestToBinarySearch();
         }
 
         private static void TestsToList() //как по узлу искать я понял, но немного не допер как проверить.
@@ -44,7 +48,38 @@ namespace Lesson2
             var tempValue = list.FindNode(14);
 
             Console.WriteLine(tempValue.Value);
+        }
 
+        private static void TestToBinarySearch()
+        {
+            Console.WriteLine(BinarySearch(new int[] {1,2,3,4,5,6},4));
+            Console.WriteLine(BinarySearch(new int[] {4,8,7,6,5,4,7,3,7 }, 4));//даже если не упадет, отработает неправильно, ибо массив не отсортирован
+            Console.WriteLine(BinarySearch(new int[] {8,7,6, 5, 4, 3, 2, 1}, 4));//отработает тоже неправильно, ибо массив отсортирован не в том порядке, который нужен
+        }
+
+        private static int BinarySearch(int[] array, int searchValue) // сложность log2(n)
+        {
+            int min = 0;
+            int max = array.Length - 1;
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == array[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < array[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+
+            return -1;
         }
     }
 }
